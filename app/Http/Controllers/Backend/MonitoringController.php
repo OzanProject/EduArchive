577143801d700df979819173921c279f2459505b
ozanproject <admin@admin.com>
2026-02-18 07:07:06 +0700

    Optimize: Fine-tuning monitoring data retrieval and audit log display

--- app/Http/Controllers/Backend/MonitoringController.php
+++ app/Http/Controllers/Backend/MonitoringController.php
@@ -38,7 +38,7 @@ class MonitoringController extends Controller
 
     $students = $query->paginate($perPage)->withQueryString();
 
-    return view('students_detail.index', compact('students'));
+    return view('backend.superadmin.monitoring.students', compact('students'));
   }
 
   public function showSchool(Request $request, $id)
